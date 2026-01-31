0106bbcfd9c8584641dcc2044cdcae1b06381604
ZaheerK-pro <jahirkhanis665@gmail.com>
2026-01-31 09:48:42 +0530

    fix: url fix

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -3,11 +3,10 @@ import { Message } from '../entities/Message.js'
 
 export const AppDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  username: process.env.DB_USER,
-  password: String(process.env.DB_PASSWORD),
-  database: process.env.DB_NAME,
+  url: process.env.DATABASE_URL,
+  ssl: process.env.NODE_ENV === 'production'
+    ? { rejectUnauthorized: false }
+    : false,
   synchronize: process.env.NODE_ENV !== 'production',
   logging: process.env.NODE_ENV === 'development',
   entities: [Message],
