ca7806c5fd1b2849937bf2d714edd21f539dac76
ZaheerK-pro <jahirkhanis665@gmail.com>
2026-01-31 10:12:48 +0530

    fix url

--- src/config/baseUrl.js
+++ src/config/baseUrl.js
@@ -1,1 +1,2 @@
-export const baseUrl = import.meta.env.VITE_API_BASE_URL ?? process.env.API_BASE_URL
+export const baseUrl =
+  import.meta.env.VITE_API_BASE_URL ?? 'https://portfolio-api-7ump.onrender.com/api'
